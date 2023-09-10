088b252a4ca2f647d127e4dc71ea63e497fa28c9
Yong Studt <1240622744@qq.com>
2023-09-10 22:21:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,9 @@ const {
         uid: USER_ID,
         type: 0,
     }).catch(error => console.error(`无法获取用户播放记录 \n${error}`));
-
+    
     const content = record.body;
+    console.log(JSON.stringify(content));
     console.log(JSON.stringify(content.weekData));
     const songId = content.weekData[0].song.id + '';
     const songLink = "https://music.163.com/#/song?id=" + songId
